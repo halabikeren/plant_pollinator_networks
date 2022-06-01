73e95ad36a3553efbf2dc9c51557e3ecce3afeef
halabikeren <halabikeren@gmail.com>
2022-06-01 13:38:56 +0300

    added retry mechanism

--- data_processing/name_resolution/name_resolver.py
+++ data_processing/name_resolution/name_resolver.py
@@ -29,13 +29,14 @@ class NameResolver:
         self.aux_dir = aux_dir
 
     def _set_env(self) -> tuple[str, str, str]:
-        input_dir = f"{self.aux_dir}/{self.method}_query_names_{time.time()}/"
-        output_dir = f"{self.aux_dir}/{self.method}_resolved_names_{time.time()}/"
-        work_dir = f"{self.aux_dir}/{self.method}_name_resolution_jobs_{time.time()}/"
+        timestamp = time.time()
+        input_dir = f"{self.aux_dir}/{self.method}_query_names_{timestamp}/"
+        output_dir = f"{self.aux_dir}/{self.method}_resolved_names_{timestamp}/"
+        work_dir = f"{self.aux_dir}/{self.method}_name_resolution_jobs_{timestamp}/"
         os.makedirs(input_dir, exist_ok=True)
         os.makedirs(output_dir, exist_ok=True)
         os.makedirs(work_dir, exist_ok=True)
-        logger.info(f"set up working environment in {work_dir} and output environment in {output_dir}")
+        logger.info(f"set up working environment in {work_dir} and output environment in {output_dir} with timestamp {timestamp}")
         return input_dir, output_dir, work_dir
 
     @staticmethod
@@ -46,7 +47,7 @@ class NameResolver:
             pd.Series(input_batches[i]).to_csv(input_path, header=["species_name"], index=False)
         logger.info(f"generated {len(input_batches)} input batches of size {batch_size}")
 
-    def _exec_name_resolution(self, input_dir: str, output_dir: str, work_dir: str, gnr_data_sources: Optional[str] = None, tnrs_data_source: Optional[str] = None, max_jobs_in_parallel: int = 30):
+    def _exec_name_resolution(self, input_dir: str, output_dir: str, work_dir: str, gnr_data_sources: Optional[str] = None, tnrs_data_source: Optional[str] = None, max_jobs_in_parallel: int = 30, max_retries_num: int = 10):
         job_commands = []
         num_batches = 0
         for path in os.listdir(input_dir):
@@ -68,13 +69,28 @@ class NameResolver:
                 ]
                 job_commands.append(commands)
         PBSService.execute_job_array(work_dir=work_dir, output_dir=output_dir, jobs_commands=job_commands, max_parallel_jobs=max_jobs_in_parallel)
-        logger.info(f"completed name resolution on {num_batches} batches, will now merge results")
+        logger.info(f"completed name resolution on {num_batches} batches, will now check for failed requests")
+
+        # now check which batches failed and retry on them, until none of the batches failed
+        failed_datasets_ids = NameResolver.get_failed_dataset_ids(log_paths=glob.glob(output_dir + f"*out", recursive=True))
+        retry_num = 1
+        while len(failed_datasets_ids) > 0 and retry_num < max_retries_num:
+            retry_num += 1
+            logger.info(f"retrying applying requests for {len(failed_datasets_ids)} batches")
+            jobs_paths = [f"{work_dir}{dataset_id}.sh" for dataset_id in failed_datasets_ids]
+            PBSService.execute_jobs(jobs_paths=jobs_paths, max_parallel_jobs=max_jobs_in_parallel)
+            failed_datasets_ids = NameResolver.get_failed_dataset_ids(
+                log_paths=glob.glob(output_dir + f"*out", recursive=True))
+
+        logger.info(f"completed name resolution on retried batches, will now merge results")
+
 
     @staticmethod
     def _unite_batch_data(read_dir: str, output_path: str):
-        dfs_paths = glob.glob(read_dir + f"**/*csv", recursive=True)
+        dfs_paths =  glob.glob(read_dir + f"**/**/output.csv", recursive=True)
         dfs = [pd.read_csv(path) for path in dfs_paths]
         df = pd.concat(dfs)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
         df.to_csv(output_path)
 
     def resolve(self, names: List[str], output_path: str, batch_size: int = 20, gnr_data_sources: Optional[str] = None, tnrs_data_source: Optional[str] = None, max_jobs_in_parallel: int = 30):
@@ -82,8 +98,8 @@ class NameResolver:
         self._get_input_batches(items=names, write_dir=input_dir, batch_size=batch_size)
         self._exec_name_resolution(input_dir=input_dir, output_dir=output_dir, work_dir=work_dir, gnr_data_sources=gnr_data_sources, tnrs_data_source=tnrs_data_source, max_jobs_in_parallel=max_jobs_in_parallel)
         self._unite_batch_data(read_dir=output_dir, output_path=output_path)
-        shutil.rmtree(input_dir, ignore_errors=True)
-        shutil.rmtree(output_dir, ignore_errors=True)
+        # shutil.rmtree(input_dir, ignore_errors=True)
+        # shutil.rmtree(output_dir, ignore_errors=True)
 
     @staticmethod
     def exec_tnrs(input_path: str, output_path: str, data_source: Optional[str] = None):
@@ -98,16 +114,16 @@ class NameResolver:
                     dfs.append(sub_df)
             df = pd.concat(dfs)
             df.rename(columns={
-                "search_string": "query",
+                "search_string": "original_name",
                 "is_approximate_match": "has_mismatches",
                 "taxon.tax_sources": "name_sources",
                 "taxon.rank": "taxon_rank",
 
             }, inplace=True)
             df["author"] = np.nan
-            df["orig_status"] = df[["taxon.synonyms", "query"]].apply(lambda row: "SYNONYM" if row["query"].capitalize() in row["taxon.synonyms"] else "ACCEPTED", axis=1)
-            df.sort_values(["query", "score"], ascending=[True, False], inplace=True)  # select matches with the best score
-            df.drop_duplicates(subset=["query"], keep='first', inplace=True)
+            df["orig_status"] = df[["taxon.synonyms", "original_name"]].apply(lambda row: "SYNONYM" if row["original_name"].capitalize() in row["taxon.synonyms"] else "ACCEPTED", axis=1)
+            df.sort_values(["original_name", "score"], ascending=[True, False], inplace=True)  # select matches with the best score
+            df.drop_duplicates(subset=["original_name"], keep='first', inplace=True)
             df.to_csv(output_path)
 
     @staticmethod
@@ -115,15 +131,15 @@ class NameResolver:
         res = os.system(
             f"Rscript --vanilla {os.path.dirname(__file__)}/wfo_name_resolution.R --input_path={input_path} --output_path={output_path}")
         df = pd.read_csv(output_path).rename(
-            columns={"spec.name.ORIG": "query",
+            columns={"spec.name.ORIG": "original_name",
                      "Fuzzy": "has_mismatches",
-                     "scientificName": "matched_name",
+                     "scientificName": "resolved_name",
                      "taxonRank": "taxon_rank",
                      "references": "name_sources",
                      "scientificNameAuthorship": "author",
                      "Old.status": "orig_status"})
         df["score"] = 1 - (df["Fuzzy.dist"] / 100)
-        df.dropna(subset=["query"], inplace=True)
+        df.dropna(subset=["original_name"], inplace=True)
         df.to_csv(output_path)
 
     @staticmethod
@@ -141,17 +157,31 @@ class NameResolver:
             os.chdir(orig_dir)
         df = pd.read_csv(base_output_path)
         df.rename(
-            columns={"query_name": "query", "data_source_title": "name_sources", "current_name_string": "matched_name"},
+            columns={"query_name": "original_name",
+                     "name_string": "matched_name",
+                     "current_name_string": "resolved_name",
+                     "data_source_title": "name_sources",},
             inplace=True)
         df["taxon_rank"] = df["classification_path_ranks"].apply(lambda rank: rank.split("|")[-1] if pd.notna(rank) else np.nan)
         df.taxon_rank = df.taxon_rank.replace(
-            {'spec.': 'species', 'gen.': 'genus', 'f.': 'form', 'var.': 'variety'})
-        df["orig_status"] = df["matched_name"].apply(lambda name: "SYNONYM" if pd.notna(name) else "ACCEPTED")
-        df["matched_name"].fillna(value=df["canonical_form"].to_dict(), inplace=True)
-        df["matched_name"].fillna(value=df["name_string"].to_dict(), inplace=True)
-        df["has_mismatches"] = np.where(df["query"] != df["matched_name"], True, False)
+            {'spec.': 'species', 'gen.': 'genus', 'f.': 'form', 'var.': 'variety', 'subsp.': 'subspecies'})
+        df["orig_status"] = df["resolved_name"].apply(lambda name: "SYNONYM" if pd.notna(name) else "ACCEPTED")
+        df["resolved_name"].fillna(value=df["matched_name"].to_dict(), inplace=True)
+        df["has_mismatches"] = np.where(df["original_name"] != df["matched_name"], True, False)
         df["author"] = np.nan
         df.to_csv(output_path)
+
+    @staticmethod
+    def get_failed_dataset_ids(log_paths: list[str]) -> list[str]:
+        failed_str = "----- Returning nothing : GNR server may be down -----"
+        failed_datasets_ids = []
+        for path in log_paths:
+            dataset_id = os.path.basename(path).replace(".out", "")
+            with open(path, "r") as log_file:
+                log_content = log_file.read()
+            if failed_str in log_content:
+                failed_datasets_ids.append(dataset_id)
+        return failed_datasets_ids
 
 
 if __name__ == '__main__':
