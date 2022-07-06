4bdddd2e8dcd1149e3313f84edc2533221733eef
halabikeren <halabikeren@gmail.com>
2022-07-06 18:42:14 +0300

    corrected folder name

---
+++ feature_computation/compute_interaction_features.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(bipartite)
+source("/groups/itay_mayrose/halabikeren/tmp/plant_pollinator_inter/feature_computation/utils.R")
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)<2) {
+  stop("Two arguments (input path and output path) must be supplied", call.=FALSE)
+}
+
+input_path = args[1]
+output_path = args[2]
+
+start_time <- Sys.time()
+interaction_features = data.frame(matrix(ncol = 7, nrow = 0))
+colnames(interaction_features) = c("network", "plant", "pollinator", "plant_dependence_on_pollinator", "plant_interactions_num", "pollinator_dependence_on_plant", "pollinator_interactions_num")
+features = get_interaction_features(input_path)
+interaction_features[nrow(interaction_features) + 1,] = c(c(basename(input_path)), features)
+write.csv(interaction_features, output_path, row.names = TRUE)
+end_time <- Sys.time()
+duration = end_time - start_time
+print(str_glue("duration = {duration}"))
---
+++ feature_computation/compute_species_features.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(bipartite)
+source("/groups/itay_mayrose/halabikeren/tmp/plant_pollinator_inter/feature_computation/utils.R")
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)<2) {
+  stop("Two arguments (input path and output path) must be supplied", call.=FALSE)
+}
+
+input_path = args[1]
+output_path = args[2]
+
+start_time <- Sys.time()
+species_features = data.frame(matrix(ncol = 4, nrow = 0))
+colnames(species_features) = c("network", "species", "rank", "nestedness_contribution")
+network_species_features = get_species_features(input_path)
+species_features = rbind(species_features, network_species_features)
+write.csv(species_features, output_path, row.names = TRUE)
+end_time <- Sys.time()
+duration = end_time - start_time
+print(str_glue("duration = {duration}"))
