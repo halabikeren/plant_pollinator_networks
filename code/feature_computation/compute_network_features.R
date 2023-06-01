4b410aef40fb2db60c4c04de90a55f1c4f921023
halabikeren <halabikeren@gmail.com>
2023-06-01 11:30:01 +0300

    added option to run single-network computation

--- code/feature_computation/compute_network_features.R
+++ code/feature_computation/compute_network_features.R
@@ -12,14 +12,27 @@ if (length(args)<2) {
 input_path = args[1]
 null_dir = args[2]
 output_path = args[3]
+only_input = args[4]
 
-start_time <- Sys.time()
-null_sim_features_path = str_replace(output_path, ".csv", "_across_null_networks.csv")
-features = get_network_features(input_path, null_sim_features_path, null_dir)
+start_time = Sys.time()
+if (only_input)
+{
+  network = process_network(input_path)
+  is_weighted = any(network > 1)
+  network_features = networklevel(web=network, weighted=is_weighted)
+  network_features[["modularity"]] = get_modularity(network)
+  extinction_features = simulate_network_extinction(network, nsim=nsim)
+  features = c(network_features, extinction_features)
+}
+else
+{
+  null_sim_features_path = str_replace(output_path, ".csv", "_across_null_networks.csv")
+  features = get_network_features(input_path, null_sim_features_path, null_dir)
+}
 features["network"] = basename(input_path)
 network_features = t(data.frame(Reduce(rbind, features)))
 colnames(network_features) = names(features)
 write.csv(network_features, output_path, row.names = TRUE)
-end_time <- Sys.time()
+end_time = Sys.time()
 duration = end_time - start_time
 print(str_glue("duration = {duration}"))
--- code/feature_computation/compute_species_features.R
+++ code/feature_computation/compute_species_features.R
@@ -12,12 +12,37 @@ if (length(args)<2) {
 input_path = args[1]
 null_dir = args[2]
 output_path = args[3]
+only_input = args[4]
 
-start_time <- Sys.time()
-null_sim_features_path = str_replace(output_path, ".csv", "_across_null_networks.csv")
-species_features = get_species_features(input_path, null_sim_features_path, null_dir, level="lower")
+start_time = Sys.time()
+if (only_input)
+{
+  network = process_network(input_path)
+  network[is.na(network)] = 0
+  is_weighted = any(network > 1)
+
+  if (level = "lower")
+  {
+    row_names = rownames(network)
+  } else {
+    row_names = colnames(network)
+  }
+  species_features = data.frame(specieslevel(web=network, level=level))
+  row.names(species_features) = row_names
+  if (level = "lower")
+  {
+    species_features["nestedness_contribution"] = get_plant_nestedness_contribution(web=network, nsimul=nsim)
+  } else {
+    species_features["nestedness_contribution"] = get_pollinator_nestedness_contribution(web=network, nsimul=nsim)
+  }
+}
+else
+{
+  null_sim_features_path = str_replace(output_path, ".csv", "_across_null_networks.csv")
+  species_features = get_species_features(input_path, null_sim_features_path, null_dir, level="lower")
+}
 species_features["network"] = basename(input_path)
 write.csv(species_features, output_path, row.names = TRUE)
-end_time <- Sys.time()
+end_time = Sys.time()
 duration = end_time - start_time
 print(str_glue("duration = {duration}"))
